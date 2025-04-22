5668bea61f22cceb77bdf44481ac500ef277fd73
flacmonster <flacmonster@users.noreply.github.com>
2025-04-22 14:44:58 -0500

    created money.js and shared method for formatting currency

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,8 @@
 //works then here you'd type cartModule.cart or
 //cartModule.addToCart.....
 import { cart, addToCart } from '../data/cart.js';
-import { products } from '../data/products.js'
+import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -32,7 +33,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
