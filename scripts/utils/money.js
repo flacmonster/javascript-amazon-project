92a9e438b1a85e8f98c194aea604181c46a6507b
flacmonster <flacmonster@users.noreply.github.com>
2025-04-24 10:51:22 -0500

    Fixing rounding error with toFixed()

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,8 @@
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  //calling round on the price before the division and ".toFixed"
+  //avoids a problem javascript sometimes has with rounding in
+  //the toFixed method
+  return (Math.round(priceCents) / 100).toFixed(2);
 };
 
 export default formatCurrency;
